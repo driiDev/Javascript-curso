9c67cd656146feeffab62a977e9ed0cdc0474750
Andrielle Soares <andriellesoares76@gmail.com>
2025-02-17 20:50:48 -0300

    feat: alteracao de variaveis

--- Aula 2/script.js
+++ Aula 2/script.js
@@ -1,20 +1,31 @@
 function adicionarTarefa() {
-    let mensagem = "Tarefa adicionada com sucesso!";
-    // pega o input do html e faz a ligação com js
-    let inputTarefa = document.getElementById("inputTarefa");
-    // pega apenas o valor do input
-    let tarefa = inputTarefa.value;
-    // exibe a mensagem na tela
-    document.getElementById("mensagem").textContent = mensagem;
-
-    let listaTarefas = document.getElementById("listaTarefas");
-    // cria a lista de tarefas
-    let novaTarefa = document.createElement("li");
-
-    // adiciona o valor do input na tela
-    novaTarefa.textContent = tarefa;
     
-    listaTarefas.appendChild(novaTarefa);
+    // pega o input do html e faz a ligação com js
+    const inputTarefa = document.getElementById("inputTarefa");
+    // pega apenas o valor do input
+    let tarefa = inputTarefa.value.trim(); // trim() retira os espaços no início e no final
+    let mensagem = document.getElementById("mensagem");
+
+    if (tarefa == "") {
+        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!!";
+        mensagem.textContent = mensagemErro;
+    } else {
+        let mensagemSucesso = "Tarefa adicionada com sucesso!";
+        mensagem.textContent = mensagemSucesso;
+      
+        // VERDE #28A745
+        // VERMELHO #A34743
+
+      // cria novo item (li) e insere na lista (ul)
+        const listaTarefas = document.getElementById("listaTarefas");
+        let novaTarefa = document.createElement("li");
+
+        // adiciona o valor do input na tela
+        novaTarefa.textContent = tarefa;
+        listaTarefas.appendChild(novaTarefa);
+    }
 
     inputTarefa.value = ""; // limpar input
+
+
   }
